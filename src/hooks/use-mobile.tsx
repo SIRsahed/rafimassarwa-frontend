edcad3c68329f6d73028c52d2615ed8541cfaecf
SIRsahed <sahedrahmanshakil@gmail.com>
2025-05-06 11:06:16 +0600

    res

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,24 +1,19 @@
-"use client"
+import * as React from "react"
 
-import { useEffect, useState } from "react"
+const MOBILE_BREAKPOINT = 768
 
-export function useMobile(breakpoint = 768) {
-  const [isMobile, setIsMobile] = useState(false)
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
-  useEffect(() => {
-    const checkScreenSize = () => {
-      setIsMobile(window.innerWidth < breakpoint)
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
+    mql.addEventListener("change", onChange)
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    return () => mql.removeEventListener("change", onChange)
+  }, [])
 
-    // Initial check
-    checkScreenSize()
-
-    // Add event listener for window resize
-    window.addEventListener("resize", checkScreenSize)
-
-    // Cleanup
-    return () => window.removeEventListener("resize", checkScreenSize)
-  }, [breakpoint])
-
-  return isMobile
+  return !!isMobile
 }
