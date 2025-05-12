b1dee4da90772bc4813fe9c0a01e566335bb42b0
SIRsahed <sahedrahmanshakil@gmail.com>
2025-05-12 12:14:46 +0600

    ai stock analysis phase 1, till breakdown

--- src/app/(website)/stock/layout.tsx
+++ src/app/(website)/stock/layout.tsx
@@ -1,3 +1,4 @@
+import BannerAds from '@/components/murakkabs_portfolio/BannerAds';
 import { OverviewSidebar } from '@/components/overview/overview-sidebar';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/toaster';
@@ -15,7 +16,8 @@ export default function ProfileLayout({ children }: { children: React.ReactNode 
                         }} />
                     </div>
                     <div className="lg:flex lg:justify-end mx-2 lg:mx-0">
-                        <div className="lg:ml-72 lg:my-20 my-8">
+                        <div className="lg:ml-72">
+                            <BannerAds />
                             {children}
                             <Toaster />
                         </div>
